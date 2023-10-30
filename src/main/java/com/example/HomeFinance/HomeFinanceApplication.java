e3cdb77c5d0bbe41139e73ca90243d09f923c3c0
SergioPL14 <sergiopina30@gmail.com>
2023-10-30 11:25:36 +0100

    First commit

---
+++ src/main/java/com/example/HomeFinance/HomeFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HomeFinance/HomeFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
